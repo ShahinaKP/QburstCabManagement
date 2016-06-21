0c5595d0f78a032518949527d6c10bf8d0a6ccd9
Shahina <shahina@qburst.com>
2016-06-21 14:14:28 +0530

    Routes configured

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,28 @@
 (function() {
  'use strict';
 
- var app = angular.module('QburstCabManagement',[]);
+ var app = angular.module('QburstCabManagement',['ngRoute']);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {
+		templateUrl: "app/partials/login.html",
+	 	controller: "LoginController"
+    })
+    // Pages
+    .when("/bookcab", {
+		templateUrl: "app/partials/bookcabDetails.html",
+	 	controller: "BookCabController"
+    })
+    .when("/triplist", {
+		templateUrl: "app/partials/tripsList.html",
+	 	controller: "TripController"
+    });
+}]);
+
+
 })();
